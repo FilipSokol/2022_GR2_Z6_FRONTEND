a1968828219757ea2670da9e7ee5db13278eb4ed
FilipSokol <sok.filip@gmail.com>
2022-10-09 19:49:30 +0200

    Add login view

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Error from "./Error";
 import Home from "./Home";
+import Login from "./Login";
 
 export default function Layout() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Login />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
---
+++ src/views/Login/index.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import styles from "./Login.module.scss";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginStatus, setLoginStatus] = useState(null);
+
+  const Login = (e) => {
+    e.preventDefault();
+    console.log(e);
+  };
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={Login} className={styles.formBox}>
+        <div className={styles.formTitle}>Logowanie</div>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Hasło"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" className={styles.loginButton}>
+          Zaloguj
+        </button>
+      </form>
+      <p className={styles.loginMessage}>{loginStatus}</p>
+    </div>
+  );
+}
